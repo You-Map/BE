9cb1fae0af70b70462632749b928bad784ad62c8
MyungJune <kevinshin123@gmail.com>
2023-06-25 13:40:28 +0900

    add urlpattern 'api-auth/' for login, logout view in browsable API

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+# Browsable API에서 login, logout view 구현 위해 추가
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
