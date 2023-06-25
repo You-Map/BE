c5e662e9216747eb3c7eb49fca617da77e916d07
sunghyun1356 <sunghyun1356@naver.com>
2023-06-25 14:44:15 +0900

    try

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'posts',
     'accounts',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.Account'
