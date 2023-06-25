33af0e8d84117373d5dc3e815ac8e274122c8297
yuyi5187 <kimyy200063@gmail.com>
2023-06-25 18:28:08 +0900

    corsheaders 설정

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'posts',
     'accounts',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CORS_ALLOWED_ORIGINS	= [
+    'http://localhost:3000',
+]
