3fe9f6f5da0259b6f425071128cf52410df15437
MyungJune <kevinshin123@gmail.com>
2023-06-25 14:38:03 +0900

    edit Post model

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    # purpose = models.ForeignKey(Purpose, on_delete=models.CASCADE)
-    # location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    purpose = models.ForeignKey(Purpose, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField()
     file = models.ImageField(upload_to='post_attachments/')
